e5d05d3c80559e4f53b558a8cde8278d6a2b4c3d
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-11-02 23:52:55 +0300

    Тест EntityRepositoryTest - в данный момент не выполняется

---
+++ spring-boot/src/test/kotlin/com/example/demo/EntityRepositoryTest.kt
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import com.example.demo.persistance.EntityRepository
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import com.example.demo.persistance.Entity
+
+@DataJpaTest
+class EntityRepositoryTest {
+    @Autowired
+    private lateinit var entityRepository: EntityRepository
+
+    @Test
+    fun `findById should find entity`() {
+        // given
+        val savedEntity = entityRepository.save(Entity(name = "name"))
+
+        // when
+        val foundEntity = entityRepository.findById(savedEntity.id!!)
+
+        // then
+        assertTrue { foundEntity.get() == savedEntity }
+    }
+}
