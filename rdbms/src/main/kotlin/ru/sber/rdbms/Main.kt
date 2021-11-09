ad9bfc6f21291995466dd4b03580fbee3c8fc577
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-11-10 00:22:07 +0300

    TransferConstraint done

--- rdbms/src/main/kotlin/ru/sber/rdbms/Main.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/Main.kt
@@ -1,12 +1,12 @@
 package ru.sber.rdbms
 
 fun main() {
-    val transferConstraint = TransferConstraint()
-    transferConstraint.transfer(1, 2, 100)
+//    val transferConstraint = TransferConstraint()
+//    transferConstraint.transfer(1, 2, 100)
 
-//    val transferOptimisticLock = TransferOptimisticLock()
-//    transferOptimisticLock.transfer(1, 2, 3)
-//
+    val transferOptimisticLock = TransferOptimisticLock()
+    transferOptimisticLock.transfer(1, 2, 100)
+
 //    val transferPessimisticLock = TransferPessimisticLock()
-//    transferPessimisticLock.transfer(1, 2, 3)
+//    transferPessimisticLock.transfer(1, 2, 100)
 }
--- rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
+++ rdbms/src/main/kotlin/ru/sber/rdbms/TransferOptimisticLock.kt
@@ -1,11 +1,31 @@
 package ru.sber.rdbms
 
+import java.sql.DriverManager
 import java.sql.SQLException
 
 class OptimisticLockException(message: String) : SQLException(message)
 
 class TransferOptimisticLock {
+    private val connection = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/db",
+        "postgres",
+        "postgres"
+    )
+
     fun transfer(accountId1: Long, accountId2: Long, amount: Long) {
-        TODO()
+        connection.use { conn ->
+            try {
+                val prepareStatement1 = conn.prepareStatement("update account3 set amount = amount + ? where id = ?;")
+                prepareStatement1.use { statement ->
+                    statement.setLong(1, amount)
+                    statement.setLong(2, accountId1)
+                    statement.executeUpdate()
+                }
+
+
+            } catch (exception: OptimisticLockException) {
+                println(exception.message)
+            }
+        }
     }
 }
